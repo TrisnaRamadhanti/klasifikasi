49da8c55f6398b356a665df2fb2d5e2ca9bb8cbf
irvanwahyu <irvanyale14@gmail.com>
2020-06-20 23:56:44 +0700

    fixing c4.5

--- outputs/rules/rules.py
+++ outputs/rules/rules.py
@@ -1,296 +1,56 @@
 def findDecision(obj): #obj[0]: peminat_prodi, obj[1]: rerata_ipk, obj[2]: kelulusan, obj[3]: jam_kehadiran_dosen, obj[4]: rerata_nilai_dosen
-   if obj[4] == '0':
-      if obj[1] == '0':
-         return 'Belum Berkembang'
-      elif obj[1] == '3.319':
-         return 'Belum Berkembang'
-      elif obj[1] == '3.112':
-         return 'Belum Berkembang'
-      elif obj[1] == '2.937':
-         return 'Belum Berkembang'
-      elif obj[1] == '2.794':
-         return 'Belum Berkembang'
-      elif obj[1] == '2.945':
-         return 'Belum Berkembang'
-      elif obj[1] == '3.234':
-         return 'Belum Berkembang'
-      elif obj[1] == '3.123':
-         return 'Belum Berkembang'
-      elif obj[1] == '2.633':
-         return 'Belum Berkembang'
-      elif obj[1] == '1.521':
-         return 'Belum Berkembang'
-      elif obj[1] == '3.018':
-         return 'Belum Berkembang'
-      elif obj[1] == '2.698':
-         return 'Belum Berkembang'
-      elif obj[1] == '19.828':
-         return 'Berkembang'
-      elif obj[1] == '3.302':
-         return 'Belum Berkembang'
-      elif obj[1] == '3.111':
-         return 'Berkembang'
-      elif obj[1] == '3.281':
-         return 'Belum Berkembang'
+   if obj[4]>0.0:
+      if obj[3]>0.0:
+         if obj[1]>0.0:
+            if obj[0]<=1.33:
+               if obj[2]<=254:
+                  return 'Berkembang'
+               else:
+                  return 'Berkembang'
+            elif obj[0]>1.33:
+               return 'Berkembang'
+            else:
+               return 'Berkembang'
+         elif obj[1]<=0.0:
+            if obj[0]<=1.33:
+               if obj[2]<=254:
+                  return 'Belum Berkembang'
+               else:
+                  return 'Belum Berkembang'
+            elif obj[0]>1.33:
+               return 'Berkembang'
+            else:
+               return 'Berkembang'
+         else:
+            return 'Belum Berkembang'
+      elif obj[3]<=0.0:
+         if obj[0]<=1.33:
+            if obj[1]>0.0:
+               if obj[2]<=254:
+                  return 'Belum Berkembang'
+               else:
+                  return 'Belum Berkembang'
+            else:
+               return 'Belum Berkembang'
+         else:
+            return 'Belum Berkembang'
       else:
          return 'Belum Berkembang'
-   elif obj[4] == '2.82':
-      if obj[0] == '0.325':
-         return 'Berkembang'
-      elif obj[0] == '0':
+   elif obj[4]<=0.0:
+      if obj[1]<=0.0:
          return 'Belum Berkembang'
+      elif obj[1]>0.0:
+         if obj[0]<=1.33:
+            if obj[2]<=254:
+               if obj[3]<=0.0:
+                  return 'Belum Berkembang'
+               else:
+                  return 'Belum Berkembang'
+            else:
+               return 'Belum Berkembang'
+         else:
+            return 'Belum Berkembang'
       else:
          return 'Belum Berkembang'
-   elif obj[4] == '3.5':
-      if obj[1] == '3.232':
-         return 'Belum Berkembang'
-      elif obj[1] == '2.105':
-         return 'Berkembang'
-      else:
-         return 'Berkembang'
-   elif obj[4] == '3.509412':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.73':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.589583':
-      return 'Berkembang'
-   elif obj[4] == '3.661765':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.35425':
-      return 'Berkembang'
-   elif obj[4] == '3.3875':
-      return 'Berkembang'
-   elif obj[4] == '3.246':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.430571':
-      return 'Berkembang'
-   elif obj[4] == '3.134699':
-      return 'Berkembang'
-   elif obj[4] == '3.02':
-      return 'Berkembang'
-   elif obj[4] == '2.112':
-      return 'Berkembang'
-   elif obj[4] == '2.936875':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.396885':
-      return 'Berkembang'
-   elif obj[4] == '2.691429':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.46933':
-      return 'Belum Berkembang'
-   elif obj[4] == '3':
-      return 'Berkembang'
-   elif obj[4] == '3.2525':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.292593':
-      return 'Berkembang'
-   elif obj[4] == '3.8':
-      return 'Berkembang'
-   elif obj[4] == '2.9448':
-      return 'Berkembang'
-   elif obj[4] == '2.7218':
-      return 'Berkembang'
-   elif obj[4] == '3.676923':
-      return 'Berkembang'
-   elif obj[4] == '3.456':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.310313':
-      return 'Berkembang'
-   elif obj[4] == '3.33125':
-      return 'Berkembang'
-   elif obj[4] == '3.246563':
-      return 'Belum Berkembang'
-   elif obj[4] == '2.821584':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.051364':
-      return 'Berkembang'
-   elif obj[4] == '3.360421':
-      return 'Berkembang'
-   elif obj[4] == '3.290345':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.349455':
-      return 'Berkembang'
-   elif obj[4] == '2.833488':
-      return 'Berkembang'
-   elif obj[4] == '3.404167':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.227462':
-      return 'Berkembang'
-   elif obj[4] == '2.613077':
-      return 'Berkembang'
-   elif obj[4] == '3.316':
-      return 'Berkembang'
-   elif obj[4] == '3.226944':
-      return 'Berkembang'
-   elif obj[4] == '3.226923':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.41':
-      return 'Berkembang'
-   elif obj[4] == '3.160488':
-      return 'Berkembang'
-   elif obj[4] == '3.27375':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.35059':
-      return 'Berkembang'
-   elif obj[4] == '3.001304':
-      return 'Berkembang'
-   elif obj[4] == '3.695217':
-      return 'Berkembang'
-   elif obj[4] == '3.035':
-      return 'Berkembang'
-   elif obj[4] == '3.30933':
-      return 'Berkembang'
-   elif obj[4] == '3.406081':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.38222':
-      return 'Berkembang'
-   elif obj[4] == '3.162333':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.544167':
-      return 'Berkembang'
-   elif obj[4] == '3.397778':
-      return 'Belum Berkembang'
-   elif obj[4] == '2.678333':
-      return 'Berkembang'
-   elif obj[4] == '3.255385':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.375515':
-      return 'Berkembang'
-   elif obj[4] == '2.976667':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.116899':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.3969':
-      return 'Berkembang'
-   elif obj[4] == '2.1226':
-      return 'Berkembang'
-   elif obj[4] == '3.2644':
-      return 'Berkembang'
-   elif obj[4] == '3.1725':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.167857':
-      return 'Berkembang'
-   elif obj[4] == '2.684783':
-      return 'Berkembang'
-   elif obj[4] == '2.436':
-      return 'Berkembang'
-   elif obj[4] == '3.27475':
-      return 'Berkembang'
-   elif obj[4] == '2.7105':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.650769':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.621818':
-      return 'Berkembang'
-   elif obj[4] == '3.70875':
-      return 'Berkembang'
-   elif obj[4] == '3.3264':
-      return 'Berkembang'
-   elif obj[4] == '3.097778':
-      return 'Berkembang'
-   elif obj[4] == '3.486418':
-      return 'Berkembang'
-   elif obj[4] == '2.625455':
-      return 'Berkembang'
-   elif obj[4] == '3.289677':
-      return 'Berkembang'
-   elif obj[4] == '2.24303':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.017763':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.010779':
-      return 'Berkembang'
-   elif obj[4] == '3.282566':
-      return 'Berkembang'
-   elif obj[4] == '3.293582':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.811538':
-      return 'Berkembang'
-   elif obj[4] == '3.9764':
-      return 'Berkembang'
-   elif obj[4] == '3.078':
-      return 'Berkembang'
-   elif obj[4] == '3.13':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.011795':
-      return 'Berkembang'
-   elif obj[4] == '2.955':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.272022':
-      return 'Berkembang'
-   elif obj[4] == '2.6196':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.42907':
-      return 'Berkembang'
-   elif obj[4] == '3.310526':
-      return 'Berkembang'
-   elif obj[4] == '3.3224':
-      return 'Berkembang'
-   elif obj[4] == '3.527143':
-      return 'Berkembang'
-   elif obj[4] == '3.13875':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.46':
-      return 'Berkembang'
-   elif obj[4] == '2.9715':
-      return 'Berkembang'
-   elif obj[4] == '2.840167':
-      return 'Berkembang'
-   elif obj[4] == '3.332':
-      return 'Berkembang'
-   elif obj[4] == '2.812154':
-      return 'Berkembang'
-   elif obj[4] == '3.3492':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.409636':
-      return 'Berkembang'
-   elif obj[4] == '3.411818':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.937273':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.266071':
-      return 'Berkembang'
-   elif obj[4] == '3.1233':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.213438':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.048056':
-      return 'Berkembang'
-   elif obj[4] == '2.985':
-      return 'Berkembang'
-   elif obj[4] == '3.31485':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.2044':
-      return 'Berkembang'
-   elif obj[4] == '3.31':
-      return 'Berkembang'
-   elif obj[4] == '2.426446':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.270286':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.145556':
-      return 'Berkembang'
-   elif obj[4] == '3.16733':
-      return 'Berkembang'
-   elif obj[4] == '3.625556':
-      return 'Berkembang'
-   elif obj[4] == '3.782':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.344286':
-      return 'Berkembang'
-   elif obj[4] == '3.547959':
-      return 'Berkembang'
-   elif obj[4] == '2.814595':
-      return 'Berkembang'
-   elif obj[4] == '3.3393':
-      return 'Berkembang'
-   elif obj[4] == '3.28875':
-      return 'Berkembang'
-   elif obj[4] == '2.812222':
-      return 'Belum Berkembang'
-   elif obj[4] == '3.52':
-      return 'Berkembang'
-   elif obj[4] == '3.3922':
-      return 'Berkembang'
    else:
-      return 'Berkembang'
+      return 'Belum Berkembang'
